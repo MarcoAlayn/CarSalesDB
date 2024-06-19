878e716fdbd21172fce8345f662b395f259a7e59
Marco Alayn <marc_rob92@hotmail.com>
2024-06-18 17:08:25 -0700

    create table customers

---
+++ 02_Table_Definitions/01_Create_Table_Customers.sql
@@ -0,0 +1,15 @@
+CREATE TABLE Customers
+(
+    CustomerID UNIQUEIDENTIFIER PRIMARY KEY,
+    FirstName VARCHAR(255),
+    LastName VARCHAR(255),
+    Email VARCHAR(255) UNIQUE,
+    PasswordHash VARCHAR(256),
+    City VARCHAR(255),
+    Address VARCHAR(255),
+    PostalCode VARCHAR(20),
+    PhoneNumber VARCHAR(20),
+    PreferredContactMethod VARCHAR(50),
+    RegistrationDate DATETIME,
+    isActive BIT
+);
